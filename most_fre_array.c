5e07b75e67a21cb3c8585b54cc24e706e7ed4b34
Apu Mandal <60982235+Hack6rapu@users.noreply.github.com>
2021-10-05 11:15:26 +0530

    Most frequent array

---
+++ most_fre_array.c
@@ -0,0 +1,39 @@
+// CPP program to find the most frequent element
+#include <bits/stdc++.h>
+using namespace std;
+
+int mostFrequent(int arr[], int n)
+{
+	sort(arr, arr + n);
+
+	int max_count = 1, res = arr[0], curr_count = 1;
+	for (int i = 1; i < n; i++) {
+		if (arr[i] == arr[i - 1])
+			curr_count++;
+		else {
+			if (curr_count > max_count) {
+				max_count = curr_count;
+				res = arr[i - 1];
+			}
+			curr_count = 1;
+		}
+	}
+
+	// If last element is most frequent
+	if (curr_count > max_count)
+	{
+		max_count = curr_count;
+		res = arr[n - 1];
+	}
+
+	return res;
+}
+
+// driver program
+int main()
+{
+	int arr[] = { 1, 5, 2, 1, 3, 2, 1 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << mostFrequent(arr, n);
+	return 0;
+}
